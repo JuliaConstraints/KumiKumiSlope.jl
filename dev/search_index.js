ddd0f3b71fd6bdff8b08aa56bbd284c6079ca1cc
Documenter.jl <documenter@juliadocs.github.io>
2021-04-08 05:26:25 +0000

    build based on 0557610

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KumiKumiSlope","category":"page"},{"location":"#KumiKumiSlope","page":"Home","title":"KumiKumiSlope","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KumiKumiSlope.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KumiKumiSlope]","category":"page"}]
+}
